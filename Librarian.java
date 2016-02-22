5275ccb7b97f4e802b1be49e1ed7ac186db0b03d
hgio <hgiovannelli@gmail.com>
2016-02-22 18:36:59 -0500

    Added files via upload

---
+++ Librarian.java
@@ -0,0 +1,12 @@
+
+public class Librarian{
+  public void removeBook(Book a){
+    a.removeBook();
+  }
+  public void addBook(Book b){
+    b.addBook();
+  }
+  public void checkHistory(Book c){
+    c.checkHistory();
+  }
+}
