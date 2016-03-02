548fc84ab0a7898050780cf8e567026518b428d7
hgio <hgiovannelli@gmail.com>
2016-03-02 15:23:31 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -6,9 +6,9 @@ public class Book{
   String genre = "";
   String author = "";
   String status = "";
-  int ISBN = 0;
+  String ISBN = "";
   boolean available = true;  //POSSIBLY NOT IN USE
-  public Book(int bISBN, String btitle, String bauthor, String bgenre, String bstatus){
+  public Book(String bISBN, String btitle, String bauthor, String bgenre, String bstatus){
       title = btitle;
       genre = bgenre;
       author = bauthor;
@@ -25,10 +25,11 @@ public class Book{
     {return author;}
   public String getStatus()
     {return status;}
-  public int getISBN()
+  public String getISBN()
     {return ISBN;}
   public boolean getAvailability()
     {return available;}
+  
   public ArrayList<Book> makeBooks(String filename){
     ArrayList<Book> books = new ArrayList<Book>();
     
@@ -37,13 +38,21 @@ public class Book{
       BufferedReader b = new BufferedReader(a);
     
       String rline = b.readLine();
+      
 
       while(rline != null){
+ //       System.out.println(rline);
         String[] str = rline.split(",");
-       // for(int i = 0;i < str.length; i++)
-        //  System.out.println(str[i]);
-        
-        books.add(new Book(Integer.parseInt(str[0].trim()),str[1],str[2],str[3],str[4]));
+//        System.out.print(str.length);
+ //       for(int i = 0;i < str.length; i++)
+ //         System.out.print(i + "," + str[i] + " AAA ");   
+        if(str.length >= 5){
+          String v = str[0];
+          String w = str[1];
+          String x = str[2];
+          String y = str[3];
+          String z = str[4];
+          books.add(new Book(v,w,x,y,z));}
         rline = b.readLine();
       }
       b.close();
@@ -57,37 +66,12 @@ public class Book{
     return books;                               
   }
   
-/*  public void removeBook(){
-    FileReader a = new FileReader("Books.txt"); //For finding line to be removed
-    BufferedReader b = new BufferedReader(a);
-    File l = new File("Books.txt"); //File to be written
-    FileReader c = new FileReader("Books.txt"); //For copying to new file
-    BufferedReader d = new BufferedReader(c);
-    FileWriter e = new FileWriter(l); //For writing to the new file
-    BufferedWriter f = new BufferedWriter(e);
-    
-    String line = b.readLine();
-    String linetotake = "";
-    while(line != null)
-      //Make sure the Book is found
-      if(line.substring(0,line.indexOf(",")).equals("" + ISBN))
-         linetotake = line;                                                                                         /// REMOVE UNTIL REQUIRED ///
-    String nline = d.readLine();
-    while (nline != null){
-      if(!nline.substring(0,line.indexOf(",")).equals(linetotake.substring(0,line.indexOf(","))))
-         f.write(nline); 
-     //Delete old file here
-    }
-    
-  }*/
-  public void addBook(){
-    
-  }
+
+
   public String toString(){
-    return ISBN + " " + title + " " + genre + " " + author + " " + status;
+    return ISBN + "," + title + "," + genre + "," + author + "," + status;
   }
-  //public String checkHistory(){ //Returns the history of a book
-  //}
+
   public void changeAvailability(){
     available = !available;
   }
