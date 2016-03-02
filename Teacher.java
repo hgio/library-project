65e6d5e995b2eb7571f0755f6d83073be22bff0b
hgio <hgiovannelli@gmail.com>
2016-03-02 15:28:54 -0500

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -34,4 +34,31 @@ public class Teacher extends Borrower
     return borrowedTitles;
   }
   
+  public static ArrayList<Teacher> makeTeachers(String filename){
+    ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+    
+    try{
+      FileReader a = new FileReader(filename);
+      BufferedReader b = new BufferedReader(a);
+    
+      String rline = b.readLine();
+
+      while(rline != null){
+        String[] str = rline.split(",");
+        
+        teachers.add(new Teacher(Integer.parseInt(str[0].trim()),str[1]));
+        rline = b.readLine();
+      }
+      b.close();
+    }
+    catch(FileNotFoundException er){
+      System.out.print("No files here!");
+    }
+    catch(IOException er){
+      System.out.print("There was a problem reading: \"" + filename + "\"");
+    }
+    return teachers;         
+  }
+  
+  
 }
